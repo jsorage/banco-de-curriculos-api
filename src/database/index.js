be6989b9d6313f90ccc3e7da745210daaaba9c56
Juliana Sorage <j.ribeiro.cavalcante@gmail.com>
2021-08-28 18:05:19 -0300

    Removido valor de MONGO_URL

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGO_URL = 'mongodb+srv://cadastrodecurriculo:desafiogama@cluster0.cvxpa.mongodb.net/jobsnet-dev?retryWrites=true&w=majority';
+const MONGO_URL = process.env.MONGO_URL;
 const MONGO_OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
 
 class Database {
